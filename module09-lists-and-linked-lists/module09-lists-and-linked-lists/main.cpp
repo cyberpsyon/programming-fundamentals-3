8cf067a1243be238a1f3e3363b176adcaa5f81cd
cyberpsyon <bjmickens@gmail.com>
2025-10-27 21:08:23 -0500

    added main.cpp

---
+++ module09-lists-and-linked-lists/module09-lists-and-linked-lists/main.cpp
@@ -0,0 +1,28 @@
+// Author:
+// Date:
+// Purpose: 
+#include "SinglyLinkedList.h"
+#include "Student.h"  // Include Student class if you want to use Student objects
+int main() {
+    //*********** Integers ***************   
+   // TODO: Create a singly linked list of integers
+    cout << "Integer List: ";
+    // TODO: Display linked list of integers
+    // TODO: Remove an element from the integer list
+    cout << "After removal: ";
+    // TODO: Display linked list of integers
+    // TODO: Clear the integer list
+    //*********** Strings ***************   
+    // TODO: Create a singly linked list of strings
+    cout << "String List: ";
+    // TODO: Display linked list of strings
+    // TODO: Clear the string list
+     //*********** Students ***************   
+    // TODO: Create a singly linked list of Student objects  
+   // TODO: Create student objects 
+   // TODO: Insert students into the list
+   // TODO: Display the student list
+    cout << "\nStudent List:" << endl;
+    // TODO: Clear the student list
+    return 0;
+}
