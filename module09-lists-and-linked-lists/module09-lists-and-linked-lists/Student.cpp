d24b328e338240ec512422aa49b4708dc08e700b
cyberpsyon <bjmickens@gmail.com>
2025-10-27 22:05:33 -0500

    Completed Student.cpp file

--- module09-lists-and-linked-lists/module09-lists-and-linked-lists/Student.cpp
+++ module09-lists-and-linked-lists/module09-lists-and-linked-lists/Student.cpp
@@ -1,13 +1,54 @@
-// Author:
-// Date:
-// Purpose: 
+// Author: Ben Mickens
+// Date: 10-26-2025
+// Purpose: Implementation of the Student class
 #include "Student.h"
 #include <iostream>
 using namespace std;
+
 // TODO: Implement the constructor to initialize the student object
+
+Student::Student(string firstname, string lastname, string BannerID, double gpa) {
+	firstname = firstname;
+	lastname = lastname;
+	BannerID = BannerID;
+	gpa = gpa;
+}
 // TODO: Implement the getter methods
+string Student::getFirstname() const {
+	return firstname;
+}
+string Student::getLastname() const {
+	return lastname;
+}
+string Student::getBannerID() const {
+	return BannerID;
+}
+double Student::getGPA() const {
+	return gpa;
+}
+
 // TODO: Implement the setter methods
+
+void Student::setFirstname(const string& firstname) {
+	firstname = firstname;
+}
+void Student::setLastname(const string& lastname) {
+	lastname = lastname;
+}
+void Student::setBannerID(const string& BannerID) {
+	BannerID = BannerID;
+}
+void Student::setGPA(double gpa) {
+	gpa = gpa;
+}
+
 // TODO: Implement the method to display student information
+
+void Student::display() const {
+	cout << "Name: " << firstname << " " << lastname << ", "
+		 << "BannerID: " << BannerID << ", "
+		 << "GPA: " << gpa << endl;
+}
 // Complete for you: Implementation of the << operator overload to print student details
 ostream& operator<<(ostream& os, const Student& student) {
     os << "Name: " << student.firstname << " " << student.lastname << ", "
